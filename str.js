09530847401504dcece1e15a3099112b51751708
hilojack <a132811@gmail.com>
2016-05-07 17:17:27 +0800

    str

--- str.js
+++ str.js
@@ -6,3 +6,19 @@ String.prototype.str_pad = function(len){
 	var ans = Array(len-this.length+1).join('0')+this
 	return ans;
 }
+/*
+String.prototype.str_pad = function(ch, n) {
+        var pad = '';
+		n = n - this.length;
+		if(n<1) return this;
+        while (true) {
+        	if (n & 1) {
+          	pad += ch;
+          }
+          n >>= 1;
+          if (n) ch += ch;
+          else break;
+        }
+        return pad+this;
+    }
+*/
