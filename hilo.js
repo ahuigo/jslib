31c59bfcf5ad9f28fce1daf5ffeef2f6a8eec5bc
yexuehui <a132811@gmail.com>
2016-06-27 19:30:14 +0800

    hilo

--- hilo.js
+++ hilo.js
@@ -33,8 +33,19 @@ function array_values(myObject) {
     return output;
 }
 
-Date.prototype.format = function(){
-    var d = this;
-    return d.getFullYear()+'-' + (d.getMonth()+1)+'-' + d.getDate()+' ' + d.getHours()+':' + d.getMinutes()+':' + d.getSeconds()+''
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "Y": ('0' + (this.getFullYear())).substr(-4), //月份 
+        "m": ('0' + (this.getMonth() + 1)).substr(-2), //月份 
+        "d": ('0' +this.getDate()) .substr(-2), //日 
+        "H": ('0' + this.getHours()).substr(-2), //小时 
+        "i": ('0'+this.getMinutes()).substr(-2), //分 
+        "s": ('0'+this.getSeconds()).substr(-2), //秒 
+        "q": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) 
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
 }
-
