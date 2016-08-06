4bcec014f9004a73f5b2cf4fd2344cec2a5ab40b
hilojack <a132811@gmail.com>
2016-08-06 18:56:57 +0800

    hilo

--- dom/table.js
+++ dom/table.js
@@ -6,59 +6,33 @@ String.prototype.encodeHtmlEntities = function(){
 		   return '&#'+k.charCodeAt(0)+';';
 		}) ;
 }
-function Table(data, insertNode){
-	this.data = data;
-	this.insertNode = insertNode;
-	if (Table._initialized === undefined) {
-		var self = Table;
-		self._initialized = true;
-		self.prototype.createTable = function(dataList, titles, insertNode) {
-			var table='';
-			table += '<tr>';
-			$.each(titles, function(k, title){
-				table +=  '<th>' + + '</th>';
-			});
-			table += '</tr>';
-
-			$('<tr>' +
-					'<th>id</th>' +
-					'<th>status</th>' +
-					'<th>uid</th>' +
-					'<th>phone</th>' +
-					'<th>渠道</th>' +
-					'<th>txt</th>' +
-					'<th>url</th>' +
-					'<th>app_title</th>' +
-					'<th>app_image_url</th>' +
-					'<th>app_content</th>' +
-					'<th>expire_time</th>' +
-					'</tr>');.appendTo('#list');
-			$.each(data['list'], function(i, item) {
-				var $tr = $('<tr id="'+item.id+'">').append(
-						$('<td>').text(item.id),
-						$('<td>').text(item.status),
-						$('<td>').text(item.uid),
-						$('<td>').text(item.phone),
-						$('<td>').text(channelName),
-						$('<td>').text(item.txt),
-						$('<td>').text(item.url),
-						$('<td>').text(item.app_title),
-						$('<td>').html('<img width="150px" src="'+item.app_image_url+'">'),
-						$('<td>').text(item.app_content),
-						$('<td>').text(
-								(new Date(item.expire_time * 1000)).toLocaleString()
-						)
-				);
-				$tr.appendTo('#list');
-			});
-			pager(page, data['maxPage']);
-			setProgress(data['task']);
-		};
-	}
-  }
-}
-	}).fail(function (xhr) {
-		$('#info').text(xhr.statuseText);
-
-	});
-}
+    function getList(page) {
+        var api = '/huajiao/badcdn/getBadCase?' + http_build_query(filterParams);
+        $.ajax(api + '&page=' + page).done(function (data) {
+            if (data['errno'] === 0) {
+                $('<tr>' +
+                    '<th>id</th>' +
+                    '<th>SN</th>' +
+                    '<th>错误类型</th>' +
+                    '<th>CDN</th>' +
+                    '<th>主播IP</th>' +
+                    '<th>时间</th>' +
+                    '</tr>').appendTo('#table');
+                $.each(data['list'], function (i, item) {
+                    var $tr = $('<tr id="' + item.id + '">').append(
+                        $('<td>').text(item.id),
+                        $('<td>').append($('<a></a>').text(item.sn).attr('href','/huajiao/livestream/detail?sn='+item.sn)),
+                        $('<td>').text(item.badType),
+                        $('<td>').text(item.deployaddress),
+                        $('<td>').text(item.inaddress),
+                        $('<td>').text(item.date)
+                    );
+                    $tr.appendTo('#table');
+                });
+                Pager($('#pager'), page, data['maxPage']);
+            } else {
+                console.log(data);
+                alerts(data['error']);
+            }
+        });
+    }
--- hilo.js
+++ hilo.js
@@ -33,20 +33,52 @@ function array_values(myObject) {
     return output;
 }
 
-/*date.js*/
-Date.prototype.Format = function (fmt) { //author: meizz 
+Date.prototype.Format = function (fmt) { //author: meizz
     var o = {
-        "Y": ('0' + (this.getFullYear())).substr(-4), //月份 
-        "m": ('0' + (this.getMonth() + 1)).substr(-2), //月份 
-        "d": ('0' +this.getDate()) .substr(-2), //日 
-        "H": ('0' + this.getHours()).substr(-2), //小时 
-        "i": ('0'+this.getMinutes()).substr(-2), //分 
-        "s": ('0'+this.getSeconds()).substr(-2), //秒 
-        "q": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
+        "Y": ('0' + (this.getFullYear())).substr(-4), //月份
+        "m": ('0' + (this.getMonth() + 1)).substr(-2), //月份
+        "d": ('0' +this.getDate()) .substr(-2), //日
+        "H": ('0' + this.getHours()).substr(-2), //小时
+        "i": ('0'+this.getMinutes()).substr(-2), //分
+        "s": ('0'+this.getSeconds()).substr(-2), //秒
+        "q": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
     };
     for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) 
+        if (new RegExp("(" + k + ")").test(fmt))
             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
+
+
+/**
+ * 初始化参数
+ */
+
+var params = location.search.parseStr();
+var filterParams = {};
+$(function(){
+    $('#filter input[type="text"],#filter input[type="checkbox"], #filter select').each(function(k, input){
+        if(input.name in params){
+            if(['radio', 'checkbox'].indexOf(input.type ) > -1  ) {
+                if(params[input.name]){
+                    $(input).prop('checked', true);
+                }
+                $(input).val([params[input.name]]);
+            }else{
+                $(input).val(params[input.name]);
+            }
+            filterParams[input.name] = params[input.name];
+        }
+    });
+});
+/**
+ * menu
+ */
+$(function(){
+    $('#nav-tabs').find('li a').each(function(){
+        if($(this).attr('href').split('?')[0] === location.pathname){
+            $(this).closest('li').addClass('active');
+        }
+    });
+});
