a1e6b311380918aac6138e99aee247f91d3a3bda
Xuehui Ye <yexuehui@momenta.ai>
2019-08-06 03:04:03 +0800

    array

--- array.js
+++ array.js
@@ -5,3 +5,13 @@ Array.prototype.chunk = function(chunk=2){
 	}
 	return temparray
 }
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
