504a015eb9812ee67a74f57566d0fd35074da913
Xuehui Ye <yexuehui@momenta.ai>
2019-09-25 22:32:13 +0800

    camera.js

---
+++ cesium/camera.js
@@ -0,0 +1,5 @@
+// Set the initial view
+viewer.scene.camera.setView({
+    destination : new Cesium.Cartesian3.fromDegrees(-73.998114468289017509, 40.674512895646692812, 2631.082799425431),
+    orientation : new Cesium.HeadingPitchRoll.fromDegrees(7.1077496389876024807, -31.987223091598949054, 0.025883251314954971306),
+});
